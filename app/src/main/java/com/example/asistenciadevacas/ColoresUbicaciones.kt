0c1b3453d687042f8d7ea4f921aa4b6853f6d713
mriveros <mriveros.py@gmail.com>
2024-08-20 23:20:34 -0400

    modified:   app/src/main/java/com/example/asistenciadevacas/ColoresUbicaciones.kt 	modified:   app/src/main/java/com/example/asistenciadevacas/ControlVaca.kt 	modified:   app/src/main/res/layout/activity_control_vaca.xml 	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/asistenciadevacas/ColoresUbicaciones.kt
+++ app/src/main/java/com/example/asistenciadevacas/ColoresUbicaciones.kt
@@ -30,5 +30,24 @@ class ColoresUbicaciones {
             "Macho",
             "Hembra"
         )
+        val tiposControles = arrayListOf(
+            "Seleccionar...",
+            "Control de Peso",
+            "Control de Salud",
+            "Control de Alimentación"
+        )
+        val controles = arrayListOf(
+            "Vacunación",
+            "Desparasitación interna",
+            "Desparasitación externa",
+            "Manejo de la mastitis",
+            "Higiene y limpieza",
+            "Corte de pezuñas",
+            "Manejo reproductivo",
+            "Sanitaciones pre y postparto",
+            "Control de enfermedades.",
+            "Nutrición y suplementación"
+        )
+
     }
 }
--- app/src/main/java/com/example/asistenciadevacas/ControlVaca.kt
+++ app/src/main/java/com/example/asistenciadevacas/ControlVaca.kt
@@ -3,7 +3,9 @@ package com.example.asistenciadevacas
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,6 +14,7 @@ class ControlVaca : AppCompatActivity() {
     companion object{
         var refrescar = 0
     }
+    lateinit var arrayControles: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +30,12 @@ class ControlVaca : AppCompatActivity() {
 
         val nombreVaca = findViewById<TextView>(R.id.txtNombreVaca)
         val caravanaVaca = findViewById<TextView>(R.id.txtNroCaravana)
+        val listaControles = ColoresUbicaciones.controles
+        val SnnControlesControles = findViewById<Spinner>(R.id.SnnTipoControl)
 
+        arrayControles = findViewById(R.id.SnnTipoControl)
+        val adaptadorTiposControlles = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,listaControles)
+        arrayControles.adapter=adaptadorTiposControlles
 
         if (vaca != null) {
             nombreVaca.text = "Nombre: " + vaca.nombre_vaca //?.toUpperCase()
